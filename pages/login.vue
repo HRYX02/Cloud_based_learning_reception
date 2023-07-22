fe625669f69316c5cfa6f748e8743babececeb12
SxxStar <2291874995@qq.com>
2023-07-22 17:36:26 +0800

    普通登录退出

--- pages/login.vue
+++ pages/login.vue
@@ -47,6 +47,9 @@
 <script>
 import '~/assets/css/sign.css'
 import '~/assets/css/iconfont.css'
+import cookie from 'js-cookie'
+import loginApi from '@/api/login'
+
 export default {
     name: 'Login',
     layout: 'sign',
@@ -61,11 +64,30 @@ export default {
     },
     methods: {
         checkPhone(rule, value, callback) {
-            //debugger
             if (!(/^1[34578]\d{9}$/.test(value))) {
                 return callback(new Error('手机号码格式不正确'))
             }
             return callback()
+        },
+        submitLogin() {
+            //第一步 调用接口进行登录，返回token字符串
+            loginApi.submitLoginUser(this.user)
+                .then(response => {
+                    // 第二步 获取token字符串放到cookie里面
+                    // 第一个参数cookie名称，第二个参数值，第三个参数作用范围
+                    cookie.set('yunShang_token', response.data.data.token, { domain: 'localhost' })
+
+                    // 第四步 调用接口 根据token获取用户信息，为了首页面显示
+                    loginApi.getLoginUserInfo()
+                        .then(response => {
+                            this.loginInfo = response.data.data.userInfo
+                            // 获取返回用户信息，放到cookie里面
+                            cookie.set('yunShang_uCenter', JSON.stringify(this.loginInfo), { domain: 'localhost' })
+
+                            // 跳转页面
+                            window.location.href = "/";
+                        })
+                })
         }
     }
 }
--- utils/request.js
+++ utils/request.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
+import cookie from 'js-cookie'
+
 // 创建axios实例
 const service = axios.create({
     // api的base_url
     baseURL: 'http://localhost:8080',
     timeout: 20000
 })
+service.interceptors.request.use(
+    config => {
+        // 判断cookie里面是否有名称为yunShang_token的数据
+        if (cookie.get('yunShang_token')) {
+            // 把获取cookie值放到header中
+            config.headers['token'] = cookie.get('yunShang_token');
+        }
+        return config
+    },
+    err => {
+        return Promise.reject(err);
+    }
+)
 export default service
