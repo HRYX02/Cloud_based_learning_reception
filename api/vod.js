ef9dbf09715d78f0a6672a879b31da25f1bc632c
SxxStar <2291874995@qq.com>
2023-07-25 16:40:10 +0800

    章节视频点播

---
+++ api/vod.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+export default {
+  getPlayAuth(id) {
+    return request({
+      url: `/vodservice/video/getPlayAuth/${id}`,
+      method: 'get'
+    })
+  }
+
+}
